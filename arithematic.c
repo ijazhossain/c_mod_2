8ca9785751328c6e9a767b52f903d9d43e32922d
ijazhossain <ijaz.hossain35@gmail.com>
2025-04-08 08:43:28 +0600

    arithematic commit

---
+++ arithematic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    float a=15;
+    int b=2;
+    int sum=a+b;
+    int sub=a-b;
+    int mul=a*b;
+    float div=a/b;
+    printf("Summation is= %d\n",sum);
+    printf("Subtraction is= %d\n",sub);
+    printf("Multiplication is= %d\n",mul);
+    printf("division is= %f\n",div);
+    return 0;
+}
